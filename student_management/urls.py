ed81fad831303adee9fa177cf2560d3eb754a611
Suraj Verma <surajverma8730448@gmail.com>
2024-02-10 16:34:18 +0530

    recommeded change apply

--- student_management/urls.py
+++ student_management/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from student.views import *
+# for use of media directory
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,5 @@ urlpatterns = [
     path('add-student/', addStudent, name="addStudent"),
     path('update-student/<str:pk>', updateStudent, name="updateStudent"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
